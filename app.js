f3c5462017a49e4000bba0c90eb91a2b77ba816d
qasims-dev <qasims.dev@gmail.com>
2019-08-18 02:18:24 +0300

    Added signup,login,dashboard,profile and logout functionality

--- app.js
+++ app.js
@@ -1,21 +1,57 @@
 const express = require("express"),
   routes = require("./routes"),
-  user = require("./routes/user");
+  user = require("./routes/user"),
+  http = require("http"),
+  path = require("path");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const ejs = require("ejs");
 const flash = require("req-flash");
-const app = express();
+var mysql = require("mysql");
 
-app.use(cookieParser());
+const app = express();
+var bodyParser = require("body-parser");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "meriYAAD1",
+  database: "ejssessionauth"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    return console.error("error: " + err.message);
+  }
+
+  //console.log("Connected to the MySQL server.");
+});
+
+global.db = connection;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/* app.use(cookieParser());
 app.use(session({ secret: "123" }));
-app.use(flash());
+app.use(flash()); */
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-
+app.use(
+  session({
+    secret: "meriyaad",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  })
+);
 app.get("/", routes.index);
 app.get("/signup", user.signup);
 app.post("/signup", user.signup);
-
+app.get("/login", routes.index);
+app.post("/login", user.login);
+app.get("/dashboard", user.dashboard);
+app.get("/profile", user.profile);
+app.get("/logout", user.logout);
 const PORT = process.env.PORT || "5000";
 app.listen(PORT, () => console.log(`Listening server on port:${PORT}`));
--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,120 @@
-exports.signup = function(req, res) {};
+//COMPONENT TO SIGNUP
+exports.signup = function(req, res) {
+  message = "";
+  errno = 0;
+  if (req.method == "POST") {
+    var post = req.body;
+    var fname = post.firstname;
+    var sname = post.secondname;
+    var email = post.email;
+    var password = post.password;
+
+    var sql =
+      "INSERT INTO `users`(`first_name`,`last_name`,`email`,`password`) VALUES('" +
+      fname +
+      "','" +
+      sname +
+      "','" +
+      email +
+      "','" +
+      password +
+      "')";
+    var query = db.query(sql, function(err, result) {
+      if (err) {
+        if (err.errno == 1062) {
+          message = "Username/Email already exists.";
+          errno = 400;
+        } else {
+          message = "Account not created.";
+          errno = 400;
+        }
+        //console.log(err.errno);
+      } else {
+        message = "Succesfully! Your account has been created.";
+        errno = 200;
+      }
+      res.render("signup", { message, errno });
+    });
+  } else {
+    errno = 0;
+    res.render("signup", { message, errno });
+  }
+};
+
+//COMPONENT TO LOGIN
+exports.login = (req, res) => {
+  message = "";
+  errno = 0;
+  var sess = req.session;
+  if (req.method == "POST") {
+    var post = req.body;
+    var email = post.email;
+    var password = post.password;
+    var sql =
+      "SELECT id,first_name,last_name FROM users WHERE email=? AND password=?";
+    db.query(sql, [email, password], (err, results) => {
+      if (err) {
+        message = "Problem with database";
+        //console.log(err);
+      } else {
+        if (results.length) {
+          message = "successful login";
+          req.session.userId = results[0].id;
+          req.session.user = results[0];
+          //console.log(results[0].id);
+          res.redirect("/dashboard");
+
+          //console.log(message);
+        } else {
+          message = "Wrong credentials";
+          errno = 400;
+          //console.log(message);
+          res.render("index", { message, errno });
+          //console.log(message);
+        }
+      }
+    });
+  } else {
+    //console.log("Else");
+    res.render("index.ejs", { message });
+  }
+};
+
+//COMPONENT OF DASHBOARD PAGE
+exports.dashboard = (req, res) => {
+  var user = req.session.user,
+    userId = req.session.userId;
+  //console.log("ddd=" + userId);
+  if (userId == null) {
+    res.redirect("/login");
+    return;
+  }
+
+  var sql = "SELECT * from users WHERE id=?";
+  db.query(sql, [userId], (err, results) => {
+    res.render("dashboard.ejs", { user });
+  });
+};
+
+//COMPONENT OF PROFILE PAGE
+exports.profile = (req, res) => {
+  var user = req.session.user,
+    userId = req.session.userId;
+  //console.log("ddd=" + userId);
+  if (userId == null) {
+    res.redirect("/login");
+    return;
+  }
+
+  var sql = "SELECT * from users WHERE id=?";
+  db.query(sql, [userId], (err, results) => {
+    res.render("profile.ejs", { data: results });
+  });
+};
+
+//COMPONENT OF LOGOUT PAGE
+exports.logout = (req, res) => {
+  req.session.destroy(function(err) {
+    res.redirect("/login");
+  });
+};
